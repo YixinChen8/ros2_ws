dbfc7f0cbe5f6b0d987b0a6f5ce3f2a51c60fb33
Yixin Chen <yixinchen3@hotmail.com>
2026-04-11 15:13:19 -0400

    updates

--- src/polygon_base/include/polygon_base/regular_polygon.hpp
+++ src/polygon_base/include/polygon_base/regular_polygon.hpp
@@ -7,6 +7,9 @@ namespace polygon_base
   class RegularPolygon
   {
     public:
+    // One thing to notice is the presence of the initialize method. 
+    // With pluginlib, a constructor without parameters is required, 
+    // so if any parameters to the class are needed, we use the initialize method to pass them to the object.
       virtual void initialize(double side_length) = 0;
       virtual double area() = 0;
       virtual ~RegularPolygon(){}
--- src/polygon_base/src/area_node.cpp
+++ src/polygon_base/src/area_node.cpp
@@ -19,10 +19,22 @@ int main(int argc, char** argv)
   (void) argc;
   (void) argv;
 
+  /*
+  The ClassLoader is the key class to understand, defined in the class_loader.hpp header file:
+  - It is templated with the base class, i.e. polygon_base::RegularPolygon.
+  - The first argument is a string for the package name of the base class, i.e. polygon_base.
+  - The second argument is a string with the fully qualified base class type for the plugin, i.e. polygon_base::RegularPolygon.
+  */
   pluginlib::ClassLoader<polygon_base::RegularPolygon> poly_loader("polygon_base", "polygon_base::RegularPolygon");
 
   try
   {
+    /*
+    There are a number of ways to instantiate an instance of the class. In this example, we’re using shared pointers. 
+    We just need to call createSharedInstance with a reference to the plugin: 
+    This can be either the fully-qualified type of the plugin class (the type attribute of the declaration XML file, e.g. polygon_plugins::Square), 
+    or the optional magic name (the name attribute of the declaration XML file, e.g., awesome_triangle).    
+    */
     std::shared_ptr<polygon_base::RegularPolygon> triangle = poly_loader.createSharedInstance("awesome_triangle");
     triangle->initialize(10.0);
 
